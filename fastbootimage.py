1dd54335e5e05676689ba63dfa75be8383c95c73
R0rt1z2 <me@r0rt1z2.com>
2024-10-15 23:44:49 +0200

    hisi-playground: Add option to specify start offset

--- fastbootimage.py
+++ fastbootimage.py
@@ -1,15 +1,17 @@
 from struct import pack, unpack
 from argparse import ArgumentParser
 
-START_OFFSET = 0x800
+DEFAULT_START_OFFSET = 0x800
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('image', help='Bootloader image (fastboot.img)')
+    parser.add_argument('--start-offset', type=lambda x: int(x, 0), default=DEFAULT_START_OFFSET, 
+                        help='Specify start offset (e.g., 0x20)')
     args = parser.parse_args()
 
     with open(args.image, 'rb') as f:
-        f.seek(START_OFFSET)
+        f.seek(args.start_offset)
         header = unpack('<I16sII', f.read(28))
 
     print('First command: 0x%08x' % header[0])
